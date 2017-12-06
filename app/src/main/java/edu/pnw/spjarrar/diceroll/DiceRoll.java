f9d82dca3ffdeecf9d9e0ac675b0d557583288bd
sjarrard <sjarrard@users.noreply.github.com>
2017-12-05 21:19:22 -0600

    Split into multiple files, made more object-oriented.

--- app/src/main/java/edu/pnw/spjarrar/diceroll/DiceRoll.java
+++ app/src/main/java/edu/pnw/spjarrar/diceroll/DiceRoll.java
@@ -1,26 +1,17 @@
 package edu.pnw.spjarrar.diceroll;
 
-        import android.os.Bundle;
-        import android.support.design.widget.FloatingActionButton;
-        import android.support.design.widget.Snackbar;
+import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
         import android.view.View;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.widget.Button;
         import android.widget.EditText;
         import android.widget.TextView;
-        import java.lang.Math;
-        import java.util.ArrayList;
-        import java.util.Random;
 
 public class DiceRoll extends AppCompatActivity {
 
     EditText startNum, endNum, customInput;
     TextView rollResult, customResult, customSum;
-
-
+    String errorMessage = "Error";
+    String shortError = "E";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,107 +24,30 @@ public class DiceRoll extends AppCompatActivity {
         customInput = findViewById(R.id.editTextCustom);
         customResult = findViewById(R.id.textViewCustom);
         customSum = findViewById(R.id.textViewSum);
-
     }
-
 
     public void roll(View view){
-        String startString, endString;
-        int startInt, endInt, rollInt;
-        boolean errorFlag = false;
-
-        Random dice = new Random();
-
-        startInt = 1;
-        endInt = 6;
-
-        startString = startNum.getText().toString();
-        endString = endNum.getText().toString();
-
-        try{
-            startInt = Integer.parseInt(startString);
-            endInt = Integer.parseInt(endString);
-        }catch(NumberFormatException e){
-            errorFlag = true;
-        }
-
-
-        if(endInt >= startInt) {
-            rollInt = dice.nextInt(endInt - startInt + 1) + startInt;
+        String startString = startNum.getText().toString();
+        String endString = endNum.getText().toString();
+        Die freshDice = new Die(startString, endString);
+        if(freshDice.isValidDie()){
+            rollResult.setText(Integer.toString(freshDice.singleRoll()));
         }
         else {
-            errorFlag = true;
-            rollInt = 0;
+            rollResult.setText(errorMessage);
         }
-        if(errorFlag)
-            rollResult.setText("Error");
-
-        else
-            rollResult.setText(Integer.toString(rollInt));
-
     }
-
-    public void customRoll(View v){
-        Random dice = new Random();
-
-        String customRollString ;
-        String outputString = "";
-        String[] customRollStringSplit = new String[2];
-        ArrayList<Integer> rollResultList = new ArrayList<Integer>();
-        int numOfDice = 1;
-        int sizeOfDice = 12;
-        int rollResultSum = 0;
-
-
-        boolean errorFlag = false;
-
-        customRollString = customInput.getText().toString();
-        if(customRollString.contains("d")){
-            customRollStringSplit = customRollString.split("d");
-        }
-        else {
-            errorFlag = true;
-        }
-
-        try{
-            numOfDice = Integer.parseInt(customRollStringSplit[0]);
-            sizeOfDice = Integer.parseInt(customRollStringSplit[1]);
-        }catch(NumberFormatException e){
-            errorFlag = true;
-        }
-        if(sizeOfDice < 1){
-            errorFlag = true;
-        }
-        if(!errorFlag){
-            for(int i = 0; i < numOfDice; i++){
-                int newRoll = dice.nextInt(sizeOfDice ) + 1;
-                rollResultList.add(newRoll);
-            }
-        }
-
-
-        for(int k: rollResultList){
-            if(outputString != ""){
-                outputString += ", ";
-            }
-            if(k>0) {
-                rollResultSum += k;
-                outputString += Integer.toString(k);
-            }
-
-        }
-        if(!errorFlag) {
-            customSum.setText(Integer.toString(rollResultSum));
-            customResult.setText(outputString);
+    public void customRoll(View v) {
+        String customRollString = customInput.getText().toString();
+        MultipleDice customDice = new MultipleDice(customRollString);
+        customDice.multiDiceRoll();
+        if(customDice.isValidCustomRoll()){
+            customSum.setText(Integer.toString(customDice.getRollResultSum()));
+            customResult.setText(customDice.getOutputString());
         }
         else{
-            customResult.setText("Error");
+            customResult.setText(errorMessage);
+            customSum.setText(shortError);
         }
     }
-    public void clearAll(View v){
-        rollResult.setText("");
-        startNum.setText("");
-        endNum.setText("");
-    }
-
 }
